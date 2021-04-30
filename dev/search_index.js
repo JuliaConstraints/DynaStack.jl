7a1b3b37f0d0b01e7ab320389a42b781254b3b92
Documenter.jl <documenter@juliadocs.github.io>
2021-04-30 08:12:20 +0000

    build based on 54efd3c

---
+++ dev/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"","page":"Home","title":"Home","text":"CurrentModule = DynaStack","category":"page"},{"location":"#DynaStack","page":"Home","title":"DynaStack","text":"","category":"section"},{"location":"","page":"Home","title":"Home","text":"Documentation for DynaStack.","category":"page"},{"location":"","page":"Home","title":"Home","text":"","category":"page"},{"location":"","page":"Home","title":"Home","text":"Modules = [DynaStack]","category":"page"}]
+}
